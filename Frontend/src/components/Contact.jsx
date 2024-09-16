860930ace0c72d0864e7815943201583cf2f3b26
Ritik Raj Gupta <raj.ritik2009@gmail.com>
2024-09-16 15:05:46 +0530

    first commit

---
+++ Frontend/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>hii ritik this side </h1>
+      <p>Welcome to our site of books with varitu=ies of books are there in it</p>
+    </div>
+  )
+}
+
+export default Contact
